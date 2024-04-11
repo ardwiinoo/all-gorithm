24dee2f5da5e046a352be07920eb638e5a90b699
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-04-12 01:04:06 +0700

    make quick sort

--- sort/sort_impl.go
+++ sort/sort_impl.go
@@ -22,7 +22,29 @@ func (sortAlgorithm *SortAlgorithmImpl) BubbleSort(arr []int) []int {
 }
 
 func (sortAlgorithm *SortAlgorithmImpl) QuickSort(arr []int) []int {
-	panic("implement me!")
+	return quickSort(arr, 0, len(arr)-1)
+}
+
+func quickSort(arr []int, low int, high int) []int {
+	if low < high {
+		pi := partition(arr, low, high)
+		quickSort(arr, low, pi-1)
+		quickSort(arr, pi+1, high)
+	}
+	return arr
+}
+
+func partition(arr []int, low int, high int) int {
+	pivot := arr[high]
+	i := low - 1
+	for j := low; j <= high-1; j++ {
+		if arr[j] < pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return i + 1
 }
 
 func (sortAlgorithm *SortAlgorithmImpl) MergeSort(arr []int) []int {
