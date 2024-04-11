2f1b614d48c9ea498272df76565ce9895da2bf4a
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-04-12 01:03:59 +0700

    use quick sort, add comment for simulation

--- main.go
+++ main.go
@@ -10,7 +10,33 @@ func main() {
 
 	sorter := sort.NewSortAlgorithm()
 
-	sortedArr := sorter.BubbleSort(arr)
+	bubbleSort := sorter.BubbleSort(arr)
+	fmt.Println("Result Bubble Sort: ", bubbleSort)
 
-	fmt.Println("Result: ", sortedArr)
+	/*
+		[4, 6, 3, 33, 9, 100] -> Compare: 4, 6
+		[4, 6, 3, 33, 9, 100] -> Compare: 6, 3
+		[4, 3, 6, 33, 9, 100] -> Compare: 6, 33
+		[4, 3, 6, 33, 9, 100] -> Compare: 33, 9
+		[4, 3, 6, 9, 33, 100] -> Compare: 33, 100
+		[4, 3, 6, 9, 33, 100] -> End of pass 1
+		[4, 3, 6, 9, 33, 100] -> Compare: 4, 3
+		[3, 4, 6, 9, 33, 100] -> Compare: 4, 6
+		[3, 4, 6, 9, 33, 100] -> Compare: 6, 9
+		[3, 4, 6, 9, 33, 100] -> Compare: 9, 33
+		[3, 4, 6, 9, 33, 100] -> Compare: 33, 100
+		[3, 4, 6, 9, 33, 100] -> End of pass 2
+		[3, 4, 6, 9, 33, 100] -> Sorted
+	*/
+
+	quickSort := sorter.QuickSort(arr)
+	fmt.Println("Result Quick Sort: ", quickSort)
+
+	/*
+		[4, 6, 3, 33, 9, 100] -> Pivot: 100
+		[4, 6, 3, 9, 33, 100] -> Pivot: 33
+		[4, 6, 3, 9, 33, 100] -> Pivot: 9
+		[3, 4, 6, 9, 33, 100] -> Sorted
+	*/
+
 }
